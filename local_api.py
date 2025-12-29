f7e432288d41d8c20f1474bc5e6ef917e3a0d754
smolfrog-13 <145129573+smolfrog-13@users.noreply.github.com>
2025-12-29 12:37:34 -0600

    Update local_api.py

--- local_api.py
+++ local_api.py
@@ -1,16 +1,13 @@
 import json
-
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+r = requests.get("http://127.0.0.1:8000/")
 
 # TODO: print the status code
-# print()
+print(f"GET request status code: {r.status_code}")
 # TODO: print the welcome message
-# print()
-
-
+print(f"Welcome message: {r.json()['message']}")
 
 data = {
     "age": 37,
@@ -30,9 +27,9 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
 
 # TODO: print the status code
-# print()
+print(f"POST request status code: {r.status_code}")
 # TODO: print the result
-# print()
+print(f"Result from POST request: {r.json()['result']}")
