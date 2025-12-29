85d2a3da8c9abef9a83e3be1fbb8ffe82058fc4d
smolfrog-13 <145129573+smolfrog-13@users.noreply.github.com>
2025-12-29 09:23:42 -0600

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,10 @@
+
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+from sklearn.ensemble  import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score  
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -20,7 +23,11 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
-    pass
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+    return model
+    
 
 
 def compute_model_metrics(y, preds):
@@ -45,14 +52,14 @@ def compute_model_metrics(y, preds):
     return precision, recall, fbeta
 
 
-def inference(model, X):
+def inference(model, X_test):
     """ Run model inferences and return the predictions.
 
     Inputs
     ------
-    model : ???
+    model : model (RandomForestClassifier)
         Trained machine learning model.
-    X : np.array
+    X_test : np.array
         Data used for prediction.
     Returns
     -------
@@ -60,7 +67,11 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
-    pass
+    preds = model.predict(X_test)
+
+    return preds
+
+
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -73,14 +84,24 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
-    pass
+    try:
+        with open(path, 'wb') as f:  # Write in binary mode
+            pickle.dump(model, f)
+    except Exception as e:
+        print(f"Model not saved. Error: {e}")
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
-    pass
+    try:
+        with open(path, 'rb') as f:  # Read in binary mode
+            model = pickle.load(f)
+        return model
+    except Exception as e:
+        print(f"Model not loaded. Error: {e}")
 
 
+#function to compute performance on a categorical slice of data 
 def performance_on_categorical_slice(
     data, column_name, slice_value, categorical_features, label, encoder, lb, model
 ):
@@ -89,6 +110,8 @@ def performance_on_categorical_slice(
     Processes the data using one hot encoding for the categorical features and a
     label binarizer for the labels. This can be used in either training or
     inference/validation.
+    
+    performance_on_categorical_slice is a function that computes the performance metrics when the value of a given feature is held fixed. E.g., for education, it would print out the model metrics for data with a particular value for education.
 
     Inputs
     ------
@@ -107,7 +130,7 @@ def performance_on_categorical_slice(
         Trained sklearn OneHotEncoder, only used if training=False.
     lb : sklearn.preprocessing._label.LabelBinarizer
         Trained sklearn LabelBinarizer, only used if training=False.
-    model : ???
+    model : model(RandomForestClassifier)
         Model used for the task.
 
     Returns
@@ -118,11 +141,20 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
+    input_data = data[data[column_name] == slice_value]
+    if input_data.empty:
+        return 0, 0, 0  # Handle empty slice case
     X_slice, y_slice, _, _ = process_data(
+        X=input_data,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb,
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
