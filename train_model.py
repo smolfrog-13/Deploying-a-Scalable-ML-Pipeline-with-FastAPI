77b6ad009cf3a7441aa830a0ad5af64b4e2bac4b
smolfrog-13 <145129573+smolfrog-13@users.noreply.github.com>
2025-12-29 16:08:03 -0600

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -13,7 +13,7 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "/mnt/c/Users/MeowMal/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
+project_path = "/mnt/c/Users/smolfrog-13/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path) # your code here
