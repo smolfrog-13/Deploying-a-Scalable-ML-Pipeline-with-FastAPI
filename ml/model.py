628d9326b11e980f9157fa43caef197fb762ec5c
smolfrog-13 <145129573+smolfrog-13@users.noreply.github.com>
2025-12-29 14:41:09 -0600

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -1,18 +1,25 @@
-import numpy as np
-import pandas as pd
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-# TODO: add necessary import
-from sklearn.ensemble import RandomForestClassifier
 from ml.data import process_data
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
 
-
-# Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
+
+    Inputs
+    ------
+    X_train : np.array
+        Training data.
+    y_train : np.array
+        Labels.
+    Returns
+    -------
+    model
+        Trained machine learning model.
     """
-    model = RandomForestClassifier(random_state=42)
+    model = RandomForestClassifier(random_state=42, n_estimators=100)
     model.fit(X_train, y_train)
     return model
 
@@ -20,6 +27,18 @@ def train_model(X_train, y_train):
 def compute_model_metrics(y, preds):
     """
     Validates the trained machine learning model using precision, recall, and F1.
+
+    Inputs
+    ------
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
     """
     fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
     precision = precision_score(y, preds, zero_division=1)
@@ -27,44 +46,96 @@ def compute_model_metrics(y, preds):
     return precision, recall, fbeta
 
 
-def inference(model, X_test):
-    """ Run model inferences and return the predictions. """
-    return model.predict(X_test)
+def inference(model, X):
+    """ Run model inferences and return the predictions.
+
+    Inputs
+    ------
+    model : RandomForest
+        Trained machine learning model.
+    X : np.array
+        Data used for prediction.
+    Returns
+    -------
+    preds : np.array
+        Predictions from the model.
+    """
+    preds = model.predict(X)
+    return preds
 
 
 def save_model(model, path):
-    """ Serializes model to a file. """
-    with open(path, "wb") as f:
-        pickle.dump(model, f)
+    """ Serializes model to a file.
+
+    Inputs
+    ------
+    model
+        Trained machine learning model or OneHotEncoder.
+    path : str
+        Path to save pickle file.
+    """
+    try: 
+        with open(path, 'wb') as file: 
+            pickle.dump(model, file)
+    except Exception as e:
+        print(f"Error saving model: {e}")
 
 
 def load_model(path):
-    """ Loads pickle file from `path` and returns it. """
-    with open(path, "rb") as f:
-        return pickle.load(f)
+    """ Loads pickle file from `path` and returns it."""
+    try:
+        with open(path, 'rb') as file: 
+            model = pickle.load(file)
+        return model
+    except Exception as e:
+        print(f"Error loading model: {e}")
 
 
 def performance_on_categorical_slice(
     data, column_name, slice_value, categorical_features, label, encoder, lb, model
 ):
-    """
-    Computes the model metrics on a slice of the data specified by a column name and value.
-    """
+    """ Computes the model metrics on a slice of the data specified by a column name and
+    Processes the data using one hot encoding for the categorical features and a
+    label binarizer for the labels. This can be used in either training or
+    inference/validation.
 
-    input_data = data[data[column_name] == slice_value]
-    if input_data.empty:
-        return 0, 0, 0
+    Inputs
+    ------
+    data : pd.DataFrame
+        Dataframe containing the features and label. Columns in `categorical_features`
+    column_name : str
+        Column containing the sliced feature.
+    slice_value : str, int, float
+        Value of the slice feature.
+    categorical_features: list
+        List containing the names of the categorical features (default=[])
+    label : str
+        Name of the label column in `X`. If None, then an empty array will be returned
+        for y (default=None)
+    encoder : sklearn.preprocessing._encoders.OneHotEncoder
+        Trained sklearn OneHotEncoder, only used if training=False.
+    lb : sklearn.preprocessing._label.LabelBinarizer
+        Trained sklearn LabelBinarizer, only used if training=False.
+    model : RandomForest
+        Model used for the task.
 
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+
+    """
+ 
     X_slice, y_slice, _, _ = process_data(
-        X=input_data,
+        data[data[column_name]==slice_value],
         categorical_features=categorical_features,
         label=label,
-        training=False,
         encoder=encoder,
         lb=lb,
+        training=False
     )
 
     preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
-
     return precision, recall, fbeta
