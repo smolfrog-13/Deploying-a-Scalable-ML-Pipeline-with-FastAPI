8318ef043b41f8ac46a47e3e2ec35da6a32df9ce
smolfrog-13 <145129573+smolfrog-13@users.noreply.github.com>
2025-12-29 09:26:56 -0600

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -12,15 +12,15 @@ from ml.model import (
     save_model,
     train_model,
 )
-# TODO: load the cencus.csv data
-project_path = "Your path here"
+# TODO: load the census.csv data
+project_path = os.getcwd()
 data_path = os.path.join(project_path, "data", "census.csv")
-print(data_path)
-data = None # your code here
+print(f"Loading data from: {data_path}")
+data = pd.read_csv(data_path) # your code here
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = None, None# Your code here
+train, test = train_test_split(data, test_size=0.20, random_state=42)# Your code here
 
 # DO NOT MODIFY
 cat_features = [
@@ -40,6 +40,10 @@ X_train, y_train, encoder, lb = process_data(
     # use the train dataset 
     # use training=True
     # do not need to pass encoder and lb as input
+    train,
+    categorical_features=cat_features,
+    label="salary",
+    training=True
     )
 
 X_test, y_test, _, _ = process_data(
@@ -52,21 +56,23 @@ X_test, y_test, _, _ = process_data(
 )
 
 # TODO: use the train_model function to train the model on the training dataset
-model = None # your code here
+model = train_model(X_train, y_train) # your code here
 
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
 save_model(model, model_path)
 encoder_path = os.path.join(project_path, "model", "encoder.pkl")
 save_model(encoder, encoder_path)
-
+print(f"Model saved to {model_path}")
+print(f"Model saved to {encoder_path}")
 # load the model
+print(f"Loading model from {model_path}")
 model = load_model(
     model_path
 ) 
 
 # TODO: use the inference function to run the model inferences on the test dataset.
-preds = None # your code here
+preds = inference(model, X_test) # your code here
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
@@ -81,7 +87,16 @@ for col in cat_features:
         p, r, fb = performance_on_categorical_slice(
             # your code here
             # use test, col and slicevalue as part of the input
+            data=test,
+            column_name=col,
+            slice_value=slicevalue,
+            categorical_features=cat_features,
+            label="salary",
+            encoder=encoder,
+            lb=lb,
+            model=model
         )
-        with open("slice_output.txt", "a") as f:
+        with open("slice_output.txt", "a") as f: 
             print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
             print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file=f)
+            #using append mode to preserve metrics across multiple runs and avoid overwriting previous results in case of code failure.
